1d96ba744c745d9349aab3d84962dec55d695c4e
Kolodiazhnyi Serhii <elkrot@gmail.com>
2018-06-08 12:09:18 +0300

    work

--- XFilesArchive.UI/ViewModel/Drive/FilesOnDriveViewModel.cs
+++ XFilesArchive.UI/ViewModel/Drive/FilesOnDriveViewModel.cs
@@ -1,7 +1,10 @@
-﻿using Prism.Events;
+﻿using System;
+using Prism.Events;
 using XFilesArchive.UI.Event;
 using XFilesArchive.UI.Services.Repositories;
 using XFilesArchive.UI.View.Services;
+using XFilesArchive.UI.Wrapper;
+using XFilesArchive.Model;
 
 namespace XFilesArchive.UI.ViewModel
 {
@@ -12,13 +15,15 @@ namespace XFilesArchive.UI.ViewModel
         private readonly IArchiveEntityRepository _repository;
         private readonly IEventAggregator _eventAggregator;
         private readonly IMessageDialogService _messageDialogService;
-        
+        private ArchiveEntityWrapper _archiveEntity;
+
 
         #region Конструктор
         public FilesOnDriveViewModel(IEventAggregator eventAggregator,
                    IMessageDialogService messageDialogService,
-                   IArchiveEntityRepository _repository)
+                   IArchiveEntityRepository repository)
         {
+            _repository = repository;
             _eventAggregator = eventAggregator;
             _messageDialogService = messageDialogService;
             _eventAggregator.GetEvent<SelectedItemChangedEvent>().Subscribe(OnSelectedItemChanged);
@@ -46,16 +51,45 @@ namespace XFilesArchive.UI.ViewModel
 
 
 
-        public void Load(int? DriveId = default(int?))
+        public void Load(int? FileOnDriveId = default(int?))
         {
-            var x = "";
+            ArchiveEntityLoad(FileOnDriveId);
+        }
+
+        private async void ArchiveEntityLoad(int? FileOnDriveId)
+        {
+            var _archEntity =  FileOnDriveId.HasValue ?
+               await  _repository.GetByIdAsync((int)FileOnDriveId) :
+                 new ArchiveEntity();
+
+            _archiveEntity = new ArchiveEntityWrapper(_archEntity);
+
+
+            //_archiveEntity.PropertyChanged += (s, e) =>
+            //{
+            //    if (e.PropertyName == nameof(_archiveEntity.IsChanged)
+            //    || e.PropertyName == nameof(_archiveEntity.IsValid))
+            //    {
+            //        InvalidateCommands();
+            //    }
+            //};
+        }
+
+        private void InvalidateCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ArchiveEntityWrapper ArchiveEntity
+        {
+            get
+            {
+                return _archiveEntity;
+            }
         }
 
 
 
-
-
-     
 
     }
      
