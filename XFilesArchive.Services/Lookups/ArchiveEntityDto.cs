01f2d9796758e8737e6ab78f60dd6381c5adde7d
Kolodiazhnyi Serhii <elkrot@gmail.com>
2018-12-03 16:34:41 +0200

    aedto

---
+++ XFilesArchive.Services/Lookups/ArchiveEntityDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XFilesArchive.Model;
+
+namespace XFilesArchive.Services.Lookups
+{
+    public class ArchiveEntityDto
+    {
+        public int ArchiveEntityKey { get; set; }
+
+        public Nullable<int> DriveId { get; set; }
+        
+        public string Title { get; set; }
+
+        public byte EntityType { get; set; }
+        
+        public string EntityPath { get; set; }
+        
+        public string EntityExtension { get; set; }
+
+        public string Description { get; set; }
+
+        public Nullable<long> FileSize { get; set; }
+
+        public virtual Drive Drive { get; set; }
+
+
+    }
+}
