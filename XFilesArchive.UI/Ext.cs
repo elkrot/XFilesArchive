30620cd0b4845fcf8e9ee4bd5a7961af6bb0e4d5
elkrot <elkrot@gmail.com>
2019-03-24 19:58:49 +0300

    xxx

---
+++ XFilesArchive.UI/Ext.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Ext
+{
+    public class E
+    {
+        public static readonly DependencyProperty IconProperty;
+
+        static E()
+        {
+            var metadata = new FrameworkPropertyMetadata(null);
+            IconProperty = DependencyProperty.RegisterAttached("Icon",
+    typeof(FrameworkElement),
+    typeof(E), metadata);
+
+        }
+
+        public static FrameworkElement GetIcon(DependencyObject obj)
+        {
+            return (FrameworkElement)obj.GetValue(IconProperty);
+        }
+
+        public static void SetIcon(DependencyObject obj, FrameworkElement value)
+        {
+            obj.SetValue(IconProperty, value);
+        }
+    }
+}
