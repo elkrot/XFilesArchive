9bec57031c47624670faa15df559b085b2e45534
elkrot <elkrot@gmail.com>
2019-07-22 06:10:54 +0300

    help

--- XFilesArchive.UI/View/Services/AddCategoryDialog.xaml.cs
+++ XFilesArchive.UI/View/Services/AddCategoryDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace XFilesArchive.UI.View.Services
     /// <summary>
     /// Логика взаимодействия для AddCategoryDialog.xaml
     /// </summary>
-    public partial class AddCategoryDialog : Window
+    public partial class AddCategoryDialog : MetroWindow
     {
         public AddCategoryDialog()
         {
